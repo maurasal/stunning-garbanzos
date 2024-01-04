2573602885045317b9f6daae8d97fb36a1611869
Deboh12 <deborah.alexander12@yahoo.com>
2024-01-03 20:18:53 -0600

    set up basic directory file structure and implemented models/index.js

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Job = require('./Job');
+const Application = require('./Application');
+const Interview = require('./Interview');
+
+User.hasMany(Application);
+Job.hasMany(Application);
+Application.belongsTo(User);
+Application.belongsTo(Job);
+Application.hasOne(Interview);
+Interview.belongsTo(Application);
+
+module.exports = { User, Job, Application, Interview };
