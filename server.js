f13d3ee6292f2f721440c5e484799a7a5f441a9c
unknown <lizzybumpers11@gmail.com>
2024-01-04 18:39:14 -0600

    added npms and updated server.js file with said npms/sequelize :)

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extened: true }));
 
-app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`)
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('App is listening on port ${PORT}'));
 });
