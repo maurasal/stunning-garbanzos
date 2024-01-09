1bb1c8f1123445a4925bcc1514db49ac6887774e
madihakhan-hub <144630720+madihakhan-hub@users.noreply.github.com>
2024-01-09 16:51:23 -0600

    added changes to controllers index

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
@@ -7,3 +8,15 @@ router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
+=======
+const express = require('express');
+const router = express.Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+
+module.exports = router;
+>>>>>>> da225b7 (added changes to controllers index)
