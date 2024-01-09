b57f215dc9105270ee7c7b2385b6ba22d8240251
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-01-09 16:00:46 -0600

    fixing file

--- Controllers/api/index.js
+++ Controllers/api/index.js
@@ -1,9 +1,8 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
 const userRoutes = require("./users");
-const jobAppRoutes = require("./jobApplicationRoutes");
+
 
 router.use("/users", userRoutes);
-router.use("/jobs", jobAppRoutes);
+
 
 module.exports = router;
