c4e1fe2a50f75491f185ee78cb3c79f3a5cda46c
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-01-09 16:35:20 -0600

    fixing debug

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const session = require("express-session");
 const sequelize = require("./config/connection");
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 // Sets up Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
