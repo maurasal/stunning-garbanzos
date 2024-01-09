0eb3f42900554febe6de0a266a0530148ecb9726
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-01-09 14:20:52 -0600

    init  dashboard and executing homeRoutes

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const apiRoutes = require('./api')
-const router = express.Router();
-router.use('/api', apiRoutes);
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
+// const dashboardRoutes = require('./dashboard-routes');
 
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+//router.use("/dashboard", dashboardRoutes);
 
 module.exports = router;
