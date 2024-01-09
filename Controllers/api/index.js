212f753a9bcd2a8b1372accc879fe67b6d7dfa8b
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-01-09 14:07:59 -0600

    adding jobRoute

--- Controllers/api/index.js
+++ Controllers/api/index.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const userRoutes = require('./users');
-
+const express = require("express");
 const router = express.Router();
+const userRoutes = require("./users");
+const jobAppRoutes = require("./jobApplicationRoutes");
 
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
+router.use("/jobs", jobAppRoutes);
 
 module.exports = router;
