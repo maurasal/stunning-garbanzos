133505f353a6a18ae19095da293a2b2f7ccc8613
unknown <lizzybumpers11@gmail.com>
2024-01-11 14:31:20 -0600

    created function to add job application

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,7 +1,26 @@
 const addInfoButton = document.getElementById("addJob");
 
-addInfoButton.addEventListener("click", (event) => {
+addInfoButton.addEventListener("click", async (event) => {
     event.preventDefault();
-
     
-});
+    const title = document.querySelector('#jobTitle').value.trim();
+    const company = document.querySelector('#companyName').value.trim();
+    const salary = document.querySelector('#salary').value.trim();
+    const status = document.querySelector('#applicationStatus').value.trim();
+    
+    if (title && company && salary && status) {
+      const response = await fetch(`/api/jobApplication`, {
+        method: 'POST',
+        body: JSON.stringify({ title, company, salary, status }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create Project');
+      }
+    }
+  });
