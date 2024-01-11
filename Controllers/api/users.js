6ed34ab1942e88b7de207d7e6df1a88399c1739b
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-01-10 20:02:14 -0600

    deleted by mistake

--- Controllers/api/users.js
+++ Controllers/api/users.js
@@ -39,6 +39,40 @@ router.post("/", async (req, res) => {
     res.status(400).json(err);
   }
 });
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res.status(400).json({ message: "Incorrect email or password" });
+      return;
+    }
+
+    const checkPassword = await userData.checkPassword(req.body.password);
+    if (!checkPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(200).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 // DELETE a user by ID
 router.delete("/:id", async (req, res) => {
