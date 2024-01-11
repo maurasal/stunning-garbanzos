62b6db09fd398569e740a176a6dd9ff6cb7f6042
unknown <lizzybumpers11@gmail.com>
2024-01-11 15:01:31 -0600

    edited the profile logic

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,26 +1,30 @@
-const addInfoButton = document.getElementById("addJob");
+// const addInfoButton = document.getElementById("addJob");
 
-addInfoButton.addEventListener("click", async (event) => {
-    event.preventDefault();
-    
-    const title = document.querySelector('#jobTitle').value.trim();
-    const company = document.querySelector('#companyName').value.trim();
-    const salary = document.querySelector('#salary').value.trim();
-    const status = document.querySelector('#applicationStatus').value.trim();
-    
-    if (title && company && salary && status) {
-      const response = await fetch(`/api/jobApplication`, {
-        method: 'POST',
-        body: JSON.stringify({ title, company, salary, status }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create Project');
-      }
+const addJobHandler =  async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#jobTitle').value.trim();
+  const company = document.querySelector('#companyName').value.trim();
+  const salary = document.querySelector('#salary').value.trim();
+  const status = document.querySelector('#applicationStatus').value.trim();
+
+  if (title && company && salary && status) {
+    const response = await fetch(`/api/jobApplications`, {
+      method: 'POST',
+      body: JSON.stringify({ title, company, salary, status }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create Project');
     }
-  });
+  }
+};
+
+document
+  .querySelector('.new-job-application')
+  .addEventListener('submit', addJobHandler)
